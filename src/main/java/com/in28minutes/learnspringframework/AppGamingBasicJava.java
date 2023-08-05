8df9bd422690577d38d667aba342234d41f0d823
Murugesan, Shrish (Chief Information Office - IT) <shrish.murugesan@thehartford.com>
2023-08-05 11:05:31 -0400

    Initial commit

---
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,9 @@
+package com.in28minutes.learnspringframework;
+
+public class AppGamingBasicJava {
+
+    public static void main(String[] args) {
+
+        var gameRunner = new GameRunner();
+    }
+}
---
+++ src/main/java/com/in28minutes/learnspringframework/GameRunner.java
@@ -0,0 +1,4 @@
+package com.in28minutes.learnspringframework;
+
+public class GameRunner {
+}
